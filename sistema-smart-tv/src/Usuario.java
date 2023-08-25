ad83c7fc6d818a89322c7d55bb5309dd24ee8cb0
anneoliveira <oliveira.annelise@gmail.com>
2023-08-25 10:58:42 -0300

    alterações 25/08

---
+++ sistema-smart-tv/src/Usuario.java
@@ -0,0 +1,27 @@
+public class Usuario {
+    public static void main(String[] args) throws Exception {
+        
+        SmartTv smartTv = new SmartTv();
+
+        System.out.println("TV Ligada ? " +smartTv.ligada);
+        System.out.println("Canal atual : " +smartTv.canal);
+        System.out.println("Volume atual : " +smartTv.volume);
+
+        smartTv.diminuirVolume(); 
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+        smartTv.aumentarVolume();
+        smartTv.diminuirVolume();
+        System.out.println("Volume atual : " +smartTv.volume);
+        
+        smartTv.aumentarCanal();
+        smartTv.aumentarCanal();
+        smartTv.aumentarCanal();
+        smartTv.diminuirCanal();
+        smartTv.definirCanal(38);
+
+
+
+
+    }
+}
