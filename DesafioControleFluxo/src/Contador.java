a837192ab9a575c6e3955b4da6ebe2e8d9dff4f2
anneoliveira <oliveira.annelise@gmail.com>
2023-09-05 15:36:08 -0300

    Desafio contador

---
+++ DesafioControleFluxo/src/Contador.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Contador {
+	public static void main(String[] args) {
+        try (Scanner terminal = new Scanner(System.in)) {
+            System.out.println("Digite o primeiro parâmetro");
+            int parametroUm = terminal.nextInt();
+            System.out.println("Digite o segundo parâmetro");
+            int parametroDois = terminal.nextInt();
+      
+            contar(parametroUm, parametroDois);
+        }
+        catch (ParametrosInvalidosException exception){
+            System.out.println("O segundo parâmetro deve ser maior que o primeiro");           
+        }
+    }
+     
+    static void contar(int parametroUm, int parametroDois) throws ParametrosInvalidosException{
+        int numero = 1;
+        int contagem = parametroDois - parametroUm;            
+            
+        if (parametroUm > parametroDois){
+            throw new ParametrosInvalidosException(); 
+        }
+
+            for ( ; numero <= contagem; ) {
+                System.out.println("Imprimindo número " + numero);
+                numero++;
+            }
+    }   
+}
